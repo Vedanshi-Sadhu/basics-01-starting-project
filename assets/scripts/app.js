93af3059e951f27086ded8e9e84b0f5569a3865c
Vedanshi-Sadhu <vedanshisadhu11@gmail.com>
2025-05-12 14:09:00 -0400

    updated

---
+++ assets/scripts/app.js
@@ -0,0 +1,7 @@
+// variables, operators, overwrite
+
+let currentResult = 0;
+currentResult = (currentResult + 10) * 3 / 2 - 1;
+let calculatonDescription = '(' + currentResult + '+ 10 ) * 3/2 -1';
+
+outputResult(currentResult, calculatonDescription);
